b23cb8355aaae36c6879b1bd7ff927d4ebefb051
yinfeng314 <zhang_xin314@163.com>
2022-05-18 16:14:25 +0800

    调用摄像头功能实现

--- app/src/main/java/com/example/applicationmaster/MainActivity.java
+++ app/src/main/java/com/example/applicationmaster/MainActivity.java
@@ -1,19 +1,33 @@
 package com.example.applicationmaster;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.core.content.FileProvider;
 
+import android.Manifest;
+import android.content.BroadcastReceiver;
+import android.content.ContentValues;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.net.ConnectivityManager;
+import android.net.Network;
+import android.net.NetworkInfo;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
 import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import java.io.File;
 import java.io.FileNotFoundException;
@@ -24,39 +38,36 @@ public class MainActivity extends AppCompatActivity {
     public static final int TAKE_PHOTO = 1;
     private ImageView picture;
     private Uri imageUri;
+    private IntentFilter intentFilter;
+    private NetworkChangeReceive networkChangeReceive;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Button takePhoto = (Button) findViewById(R.id.Button1);         //拍照按钮激活
-        Button trans = (Button) findViewById(R.id.trans);           //转移按钮激活
-        picture = (ImageView) findViewById(R.id.imageView);         //显示图片激活
+
+        Button takePhoto = (Button) findViewById(R.id.Button1);         // 激活拍照按钮
+        Button trans = (Button) findViewById(R.id.trans);           // 激活转移按钮
+        picture = (ImageView) findViewById(R.id.imageView);         // 激活图片显示
+
+        intentFilter = new IntentFilter();
+        intentFilter.addAction("android.net.conn.CONNECTIVITY_CHANGE");
+        networkChangeReceive = new NetworkChangeReceive();
+        registerReceiver(networkChangeReceive, intentFilter);
+
         // 拍照按钮的点击事件
         takePhoto.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                //创建file对象，用于存储拍照后的图片
-                File outputImage = new File(getExternalCacheDir(),"output_image.jpg");
-                try {
-                    if (outputImage.exists()){
-                        outputImage.delete();
-                    }
-                    outputImage.createNewFile();
-                }catch (IOException e){
-                    e.printStackTrace();
-                }
-                if (Build.VERSION.SDK_INT >= 24){
-                    imageUri = FileProvider.getUriForFile(MainActivity.this,
-                            "com.example.cameraalbumtest.fileprovider",outputImage);
+                if (ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.CAMERA)
+                != PackageManager.PERMISSION_GRANTED){
+                    ActivityCompat.requestPermissions(MainActivity.this,
+                            new String[]{Manifest.permission.CAMERA}, 1);
                 }else {
-                    imageUri = Uri.fromFile(outputImage);
+                    photo();
                 }
-                // 启动相机程序
-                Intent intent = new Intent("android.media.action.IMAGE_CAPTURE");
-                intent.putExtra(MediaStore.EXTRA_OUTPUT, imageUri);
-                startActivityForResult(intent, TAKE_PHOTO);
             }
         });
+
         // 转移按钮的点击事件
         trans.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -66,6 +77,54 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+    }
+
+    // 拍照按钮封装
+    private void photo(){
+        //创建file对象，用于存储拍照后的图片
+        File outputImage = new File(getExternalCacheDir(),"output_image.jpg");
+        try {
+            if (outputImage.exists()){
+                outputImage.delete();
+            }
+            outputImage.createNewFile();
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+        if (Build.VERSION.SDK_INT >= 24){
+            imageUri = FileProvider.getUriForFile(MainActivity.this,
+                    "com.example.applicationmaster",outputImage);
+        }else {
+            imageUri = Uri.fromFile(outputImage);
+        }
+        // 启动相机程序
+        Intent intent = new Intent("android.media.action.IMAGE_CAPTURE");
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, imageUri);
+        startActivityForResult(intent, TAKE_PHOTO);
+    }
+
+    // 权限保存
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode){
+            case 1:
+                if (grantResults.length > 0 && grantResults[0]==PackageManager.PERMISSION_GRANTED){
+                    photo();
+                }else{
+                    Toast.makeText(this, "You denied the permission", Toast.LENGTH_SHORT).show();
+                }
+                break;
+            default:
+                break;
+        }
+    }
+
+    // 通知注册事件
+    @Override
+    protected void onDestroy(){
+        super.onDestroy();
+        unregisterReceiver(networkChangeReceive);
     }
 
     @Override
@@ -85,6 +144,21 @@ public class MainActivity extends AppCompatActivity {
                 break;
             default:
                 break;
+        }
+    }
+
+    // 网络改变监测
+    class NetworkChangeReceive extends BroadcastReceiver{
+        @Override
+        public void onReceive(Context context, Intent intent){
+            ConnectivityManager connectivityManager = (ConnectivityManager)
+                    getSystemService(Context.CONNECTIVITY_SERVICE);
+            NetworkInfo networkInfo = connectivityManager.getActiveNetworkInfo();
+            if (networkInfo != null && networkInfo.isAvailable()){
+                Toast.makeText(context, "network is available", Toast.LENGTH_SHORT).show();
+            }else{
+                Toast.makeText(context,"network is unavailable", Toast.LENGTH_SHORT).show();
+            }
         }
     }
 }
